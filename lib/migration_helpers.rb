b3919c271d068f3e7a9e2b493e6960f45aec547a
christian <christian@1ed5ca13-5fb8-644a-9ef7-ad08ef2e8add>
2008-10-11 23:05:37 +0000

    Importing plugin
    git-svn-id: https://svn.cjohansen.no/rails/plugins/migration_helpers/trunk@2573 1ed5ca13-5fb8-644a-9ef7-ad08ef2e8add

---
+++ lib/migration_helpers.rb
@@ -0,0 +1,54 @@
+#
+# A set of helpful methods for use with migrations
+#
+# Christian Johansen (christian@ixd.no)
+# 2008-06-14
+#
+module MigrationHelpers
+
+  #
+  # Add a foreign key restraint for MySQL tables.
+  #
+  # Normally it is not needed to provide the from_column parameter. The column
+  # name will be guess from a singularized version with the string _id appended.
+  #
+  # In the following example the column name is assumed to be address_id
+  #
+  #   add_foreign_key :companies, :addresses
+  #
+  # The field in the receiving table is assumed to be the part of the
+  # from_column after the last _. In the example above this amounts
+  # to id. Another example:
+  #
+  #   add_foreign_key :companies, :addresses, :location_type
+  #
+  # Allthough contrived, this will result in:
+  #
+  #   alter table companies add constraint [NAME] foreign_key location_type references addresses(type)
+  #
+  def add_foreign_key(from_table, to_table, from_column = nil)
+    from_column ||= "#{to_table.to_s.singularize}_id"
+    to_column = from_column.to_s.split('_').last
+    constraint_name = "fk_#{from_table}_#{from_column}"
+
+    exec_fk "alter table #{from_table} add constraint #{constraint_name} " +
+      "foreign key (#{from_column}) references #{to_table}(#{to_column})"
+  end
+
+  #
+  # Remove a foreign key restraint
+  #
+  def remove_foreign_key(from_table, from_column)
+    constraint_name = "fk_#{from_table}_#{from_column}"
+
+    exec_fk "alter table #{from_table} drop foreign key #{constraint_name}"
+  end
+
+  #
+  # Executes sql statements. May be overriden when tested to check that
+  # statements are correctly created
+  #
+  def exec_fk(stmt)
+    execute stmt
+  end
+end
